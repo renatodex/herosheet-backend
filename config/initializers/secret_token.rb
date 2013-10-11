f85252f70d4285be52e33afd0dbc75efc0dc159d
renatodex <renatodex@gmail.com>
2013-10-10 22:07:12 -0300

    First commit!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Herosheet::Application.config.secret_token = '8fafd22754957e9e85d1b8c50a31da354cb5dbd3cb98d02f20c13cfd340285f2c3f74581100f53b0b082f4e553a479750cb64f7d37f1125a24b0d8091ac92f92'
